9aad64dd18eed84e9b0d15922454295f9f577088
Nihat Uslu <nihatyusufuslu@gmail.com>
2023-12-25 20:06:59 +0300

    home page carousel done.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,142 @@
 import Image from "next/image";
 import Slider from "./ui/slider";
+import Carousel from "./ui/homepage/carousel";
 
 export default function Home() {
-  return <main className="container flex"></main>;
+  const carouselData = [
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+    {
+      title: "Baba merabamerabamerabamerabamerabamerabamerabameraba",
+      category: "Finans",
+      img: "https://mo.ciner.com.tr/showtv/iu/400x400/show-tv-ana-haber.jpg?v=1695713446?v=438",
+    },
+  ];
+  return (
+    <main className="container flex flex-wrap">
+      <Carousel title="Spor" data={carouselData}></Carousel>
+      <Carousel title="Spor" data={carouselData}></Carousel>
+      <Carousel title="Spor" data={carouselData}></Carousel>
+      <Carousel title="Spor" data={carouselData}></Carousel>
+      <Carousel title="Spor" data={carouselData}></Carousel>
+    </main>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["mo.ciner.com.tr"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
